6a606d8775eb2be29c4bce5209325ae9f9791766
Janet Shitandi <janetshitandi99@gmail.com>
2023-07-14 13:59:49 -0700

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - my entry pont
+ * Return: always return 1 (fail)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
+
