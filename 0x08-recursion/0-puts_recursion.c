5f27397fb2d48b9762a0d1e0b393b8fb310f420f
Janet Shitandi <janetshitandi99@gmail.com>
2023-08-03 12:44:36 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * 0-puts_recursion - it is a function that writes string
+ * _puts_recursion - it is a function that writes string
  * @s: our character input
  * Return: 0 is always success
  */
@@ -10,7 +10,7 @@ void _puts_recursion(char *s)
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s +  1);
 	}
 	else
 		_putchar('\n');
