d94704c68b1137b905ef7fa69aa9832ee73fc541
Janet Shitandi <janetshitandi99@gmail.com>
2023-08-29 04:07:36 -0700

    updated

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 /**
  * free_listint - main function
- * @head - pointer
+ * @head: pointer
  */
 void free_listint(listint_t *head)
 {
 	while (head != 0)
 	{
 		listint_t *temp = head;
+
 		head = head->next;
 		free(temp);
 	}
